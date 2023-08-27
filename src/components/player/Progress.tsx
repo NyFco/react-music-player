963dbcd6364e61a16128644528330dac58ca1b10
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2023-08-27 11:37:27 +0330

    use range input

--- src/components/player/Progress.tsx
+++ src/components/player/Progress.tsx
@@ -1,13 +1,29 @@
 interface Props {
-  calcPercentage: (event: React.MouseEvent<HTMLDivElement>) => number;
   spent: number;
-  setSpent: (value: number) => void;
+  max: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  spentChangeHandle: (event: any) => void;
 }
 
-const Progress = ({ calcPercentage, spent, setSpent }: Props) => {
+const Progress = ({ spent, spentChangeHandle, max }: Props) => {
+  const timeCalc = (seconds: number): string => {
+    const min = Math.floor(seconds / 60);
+    const sec = Math.floor(seconds - min * 60);
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+  };
+
   return (
-    <div id="progress" onClick={(e) => setSpent(calcPercentage(e))}>
-      <div id="spent" style={{ width: `${spent}%` }} />
+    <div id="progress-container">
+      <h2>{timeCalc(spent)}</h2>
+      <input
+        id="progress"
+        type="range"
+        min={0}
+        max={max}
+        value={spent}
+        onChange={spentChangeHandle}
+      />
+      <h2>{timeCalc(max)}</h2>
     </div>
   );
 };
